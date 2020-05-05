a66da5ef223dfa10a550f8b2147c60ea5e3e4ead
Jose Doval <dovaldam108@gmail.com>
2020-05-05 05:22:33 +0200

    desde casa 04/05/2020 - Ejercicio nº 3

---
+++ src/main/java/dam108t3_pooavanzada/dam108t3e03.java
@@ -0,0 +1,16 @@
+package dam108t3_pooavanzada;
+
+public class dam108t3e03 {
+
+    public static void main(String[] args) {
+        System.out.println("Integer minimo: "+Integer.MIN_VALUE);
+        System.out.println("Integer maximo: "+Integer.MAX_VALUE);
+        System.out.println("Integer Bytes: "+Integer.BYTES);
+//        System.out.println("Integer Bytes: "+Integer.SIZE/8d);
+        System.out.println("Long minimo: "+Long.MIN_VALUE);
+        System.out.println("Long maximo: "+Long.MAX_VALUE);
+        System.out.println("Long Bytes: "+Long.BYTES);
+//        System.out.println("Integer Bytes: "+Long.SIZE/8d);
+    }
+    
+}
